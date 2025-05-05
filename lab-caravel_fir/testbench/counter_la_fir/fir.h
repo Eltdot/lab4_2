e6d75e13042e95a90729723a7a43a793bb3139af
Eltdot <kevinlin920910@gmail.com>
2025-05-05 17:38:28 +0800

    .h update

--- lab-caravel_fir/testbench/counter_la_fir/fir.h
+++ lab-caravel_fir/testbench/counter_la_fir/fir.h
@@ -6,19 +6,17 @@
 #define N 11
 #define data_length 64
 
-// MMIO register addresses（保留變數名稱）
 #define reg_fir_control  0x30000000
 #define reg_data_length  0x30000010
 #define reg_tap_number   0x30000014
 #define fir_x            0x30000040
 #define fir_y            0x30000044
-#define reg_fir_coeff    0x30000080  // base address for tap coefficients
+#define reg_fir_coeff    0x30000080  
 
-// MMIO access macros
 #define write_reg(addr, data) (*(volatile uint32_t*)(addr) = (data))
 #define read_reg(addr)        (*(volatile uint32_t*)(addr))
 
-// Data buffers
+
 int taps[N] = {0, -10, -9, 23, 56, 63, 56, 23, -9, -10, 0};
 int inputbuffer[N];
 int outputsignal[N];
